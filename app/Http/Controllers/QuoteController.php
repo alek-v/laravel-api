b229b4936ae23da85960e31afb15a15339b44015
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-06-16 15:22:20 +0200

    feat: Create a seeder for quotes

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quote;
+
+class QuoteController extends Controller
+{
+    public function random()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', function () { return view('index'); });
+Route::get('quotes/random', [QuoteController::class, 'random']);
