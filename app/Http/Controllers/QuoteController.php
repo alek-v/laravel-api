c0e21ff769acb83b74159434db6298c7f5401067
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-06-16 16:10:20 +0200

    feat: Show random quote

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -6,8 +6,11 @@ use App\Models\Quote;
 
 class QuoteController extends Controller
 {
+    /**
+     * Return random quote in JSON format
+     */
     public function random()
     {
-        
+        return Quote::all()->random();
     }
 }
--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Quote extends Model
 {
     use HasFactory;
+
+    protected $hidden = ['created_at', 'updated_at'];
 }
